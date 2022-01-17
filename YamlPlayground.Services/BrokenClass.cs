e05ab26d3700fe720cce0a569187ea7b590642ec
Jim Banning <jbanning1981@users.noreply.github.com>
2022-01-17 04:05:39 -0600

    Checking in broken code to test CI

---
+++ YamlPlayground.Services/BrokenClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YamlPlayground.Services
+{
+    class BrokenClass
+    {
+//Intentionally breaking compilation to test CI
+        aaa
+    }
+}
